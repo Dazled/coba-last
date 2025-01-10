b7c92ecb96016c42b49b278552f0fed2ea634c42
Dazled <danielw0703@gmail.com>
2025-01-10 19:01:22 +0700

    Update api.js

--- api.js
+++ api.js
@@ -7,19 +7,19 @@ axios.defaults.baseURL = "https://api.rajaongkir.com/starter";
 axios.defaults.headers.common['key']='33b05a2a8e3a5bf53a1f88cb3e082009';
 axios.defaults.headers.post['Content-Type']='application/x-www-form-urlencoded';
 
-router.get('/api/provinsi',(req,res) => {
+router.get('/provinsi',(req,res) => {
     axios.get('/province')
         .then( response =>  res.json(response.data))
         .catch( err => res.send(err))
       
 } )
-router.get('/api/kota/:provId',(req,res) => {
+router.get('/kota/:provId',(req,res) => {
     const id = req.params.provId;
     axios.get(`/city?province=${id}`)
         .then( response =>  res.json(response.data))
         .catch( err => res.send(err))
 })
-router.get('/api/ongkos/:asal/:tujuan/:berat/:kurir',(req,res) => {
+router.get('/ongkos/:asal/:tujuan/:berat/:kurir',(req,res) => {
     const params = req.params;
     axios.post('/cost',{
         origin:params.asal,
